e00da573891d6960c4bc06047a53f58fc3817895
Martina G.V <mgarciavior@gmail.com>
2023-07-19 18:29:34 -0300

    rediseño completo sección info (nuevo navbar y footer)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const btnmenu = document.querySelector("#btnmenu");
-const menu = document.querySelector("#menu");
+// const btnmenu = document.querySelector("#btnmenu");
+// const menu = document.querySelector("#menu");
 
-btnmenu.addEventListener("click", function () {
-  menu.classList.toggle("mostrar");
-});
+// btnmenu.addEventListener("click", function () {
+//   menu.classList.toggle("mostrar");
+// });
 
-const submenubtn = document.querySelectorAll(".submenu-btn");
-for (let i = 0; i < submenubtn.length; i++) {
-  submenubtn[i].addEventListener("click", function () {
-    if (window.innerWidth < 1024) {
-      const submenu = this.nextElementSibling;
-      const height = submenu.scrollHeight;
-      if (submenu.classList.contains("desplegar")) {
-        submenu.classList.remove("desplegar");
-        submenu.removeAttribute("style");
-      } else {
-        submenu.classList.add("desplegar");
-        submenu.style.height = height + "px";
-      }
-    }
-  });
-}
+// const submenubtn = document.querySelectorAll(".submenu-btn");
+// for (let i = 0; i < submenubtn.length; i++) {
+//   submenubtn[i].addEventListener("click", function () {
+//     if (window.innerWidth < 1024) {
+//       const submenu = this.nextElementSibling;
+//       const height = submenu.scrollHeight;
+//       if (submenu.classList.contains("desplegar")) {
+//         submenu.classList.remove("desplegar");
+//         submenu.removeAttribute("style");
+//       } else {
+//         submenu.classList.add("desplegar");
+//         submenu.style.height = height + "px";
+//       }
+//     }
+//   });
+// }
 
 
 let list = document.querySelector(".slider .list");
@@ -73,4 +73,3 @@ dots.forEach((li, key) => {
     reloadSlider();
   });
 });
-
